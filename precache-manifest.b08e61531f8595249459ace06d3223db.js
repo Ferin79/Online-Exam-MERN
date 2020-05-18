d37d0f959800663b4c82a52460313da614bda22b
Ferin <ferin@localhost.localdomain>
2020-05-18 12:05:45 +0530

    Updates

---
+++ precache-manifest.b08e61531f8595249459ace06d3223db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44327923ab8659537bac47696deef01",
+    "url": "/Ferin79/Online-Exam-MERN/index.html"
+  },
+  {
+    "revision": "e7d5629784e82b9ec4e2",
+    "url": "/Ferin79/Online-Exam-MERN/static/css/main.fc5eb00d.chunk.css"
+  },
+  {
+    "revision": "45a6e3f979789ea05aaf",
+    "url": "/Ferin79/Online-Exam-MERN/static/js/2.cf9f8eb7.chunk.js"
+  },
+  {
+    "revision": "8b2ac7c4b69492008217ba4ee133a3bd",
+    "url": "/Ferin79/Online-Exam-MERN/static/js/2.cf9f8eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d5629784e82b9ec4e2",
+    "url": "/Ferin79/Online-Exam-MERN/static/js/main.8ce79bda.chunk.js"
+  },
+  {
+    "revision": "73dd20f21986b0c01e84",
+    "url": "/Ferin79/Online-Exam-MERN/static/js/runtime-main.2671d1cd.js"
+  },
+  {
+    "revision": "f05dd86044e7015a7cfc0627361df3ed",
+    "url": "/Ferin79/Online-Exam-MERN/static/media/grade.f05dd860.png"
+  },
+  {
+    "revision": "159ae1c5050fd05506c546d33f093846",
+    "url": "/Ferin79/Online-Exam-MERN/static/media/homepage.159ae1c5.svg"
+  },
+  {
+    "revision": "0c3acbae18adcdb4a76be04703a0beff",
+    "url": "/Ferin79/Online-Exam-MERN/static/media/teacher.0c3acbae.png"
+  }
+]);
